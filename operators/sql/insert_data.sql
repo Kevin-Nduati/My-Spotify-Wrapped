f6f2b88e3f010f8628e39f59d0dfd24ccc054568
kevin-nduati <nduatikevin1@gmail.com>
2023-03-16 01:11:07 +0300

    fix xcom

--- operators/sql/insert_data.sql
+++ operators/sql/insert_data.sql
@@ -1,2 +1,6 @@
-INSERT INTO spotify_data (name, track_id, artist_name, artist_id, release_date, popularity)
-        VALUES (%s, %s, %s, %s, %s, %s)
+INSERT INTO tracks (track_id, track_name, album_id, artist_id, song_duration_ms, is_explicit, popularity, played_at)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    INSERT INTO albums (album_id, album_name, release_date)
+    VALUES (%s, %s, %s);
+    INSERT INTO artists (artist_id, artist_name)
+    VALUES (%s, %s)
