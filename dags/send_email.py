688fd3984e55f7b311355a761d5e4dccfb579638
kevin-nduati <nduatikevin1@gmail.com>
2023-04-11 14:16:55 +0300

    super linter

--- dags/send_email.py
+++ dags/send_email.py
@@ -9,56 +9,63 @@ from airflow.utils.dates import days_ago
 
 from airflow import DAG
 
-spotify_wrapped_image = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'operators/dashboard', 'spotify_wrapped.png'))
-dashboard = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'operators', 'dashboard','dashboard.py'))
+spotify_wrapped_image = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), "..", "operators/dashboard", "spotify_wrapped.png"
+    )
+)
+dashboard = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), "..", "operators", "dashboard", "dashboard.py"
+    )
+)
 print(dashboard)
-with open(spotify_wrapped_image, 'rb') as f:
+with open(spotify_wrapped_image, "rb") as f:
     image_content = f.read()
-    image_base64 = base64.b64encode(image_content).decode('utf-8')
-
+    image_base64 = base64.b64encode(image_content).decode("utf-8")
 
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email': ['nduatikevin1@gamil.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=0.25)
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email": ["nduatikevin1@gamil.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=0.25),
 }
 
 dag = DAG(
-    'spotify_wrapped_email',
+    "spotify_wrapped_email",
     default_args=default_args,
-    description='Your spotify wrapped for the past week',
+    description="Your spotify wrapped for the past week",
     schedule_interval=timedelta(weeks=1),
     start_date=days_ago(1),
-    catchup=False
+    catchup=False,
 )
 
 run_dashboard = BashOperator(
-    task_id = "run_dashboard",
-    bash_command = "python3 '{0}'".format(dashboard),
-    dag = dag
-
-
+    task_id="run_dashboard", bash_command="python3 '{0}'".format(dashboard), dag=dag
 )
 
-dashboard = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'operators', 'dashboard','dashboard.py'))
+dashboard = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), "..", "operators", "dashboard", "dashboard.py"
+    )
+)
 
 
-send_email_notification= EmailOperator(
+send_email_notification = EmailOperator(
     task_id="send_email",
-    to= "nduatikevin1@gmail.com",
+    to="nduatikevin1@gmail.com",
     subject="Your Spotify Wrapped for the past week",
     files=[spotify_wrapped_image],
     html_content="""
     <h2>Hey, There!!</h2>
     Here is your Spotify Wrapped for the past week.<br><br>
     <img src='cid:spotify_wrapped.png', height="2000px", width="2000px">
-    """   ,
-    dag = dag
+    """,
+    dag=dag,
 )
 
 run_dashboard >> send_email_notification
--- dags/songs.py
+++ dags/songs.py
@@ -1,73 +1,74 @@
 import sys
 import os
-
-from airflow import DAG
-from airflow.hooks.base import BaseHook
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
-from airflow.operators.postgres_operator import PostgresOperator
-from airflow.providers.http.operators.http import SimpleHttpOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.utils.dates import days_ago
-
-from datetime import datetime, timedelta
-import time
-
-
-# Get the parent directory of the current file (which should be the "dags" folder)
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'operators')))
 import postgres_connect
 from python_callables.collect_songs import recently_played_songs
 from python_callables.insert_songs import load_json_to_postgres
 from python_callables.last_played_at import last_song
-refresh_token = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'operators', 'refresh.py'))
-dbt_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'operators', 'dbt', "spotify_wrapped"))
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import date
+
+from datetime import timedelta
+import time
+
+
+# Get the parent directory of the current file ( "dags" folder)
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                           os.pardir))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "operators"))
+)
+
+
+refresh_token = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "operators", "refresh.py")
+)
+dbt_folder = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "operators", "dbt", "spotify_wrapped")
+)
 
 postgres_connection = postgres_connect.ConnectPostgres().postgres_connector()
 
 
 def get_next_execution_date():
-    last_play_time = last_song(postgres_conn_id = "postgres_conn")
+    last_play_time = last_song(postgres_conn_id="postgres_conn")
 
     return last_play_time
 
+
 def get_recently_played_songs(postgres_conn_id, **context):
     last_play_time = get_next_execution_date()
-    execution_date = context['execution_date']
+    execution_date = context["execution_date"]
     if last_play_time is None:
-        date_string = execution_date.strftime('%Y-%m-%d %H:%M:%S.%f')
+        date_string = execution_date.strftime("%Y-%m-%d %H:%M:%S.%f")
         print(f"The database is empty. So we will use {date_string}")
-        timestamp = int(time.mktime(time.strptime(date_string, '%Y-%m-%d %H:%M:%S.%f')))
+        timestamp = int(time.mktime(time.strptime(date_string,
+                                                   "%Y-%m-%d %H:%M:%S.%f")))
         data = recently_played_songs(start_time=timestamp)
         ti = context["ti"]
         ti.xcom_push(key="my_data", value=data)
     else:
-        date_string = last_play_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+        date_string = last_play_time.strftime("%Y-%m-%d %H:%M:%S.%f")
         print(f"The last played song is: {date_string}")
-        timestamp = int(time.mktime(time.strptime(date_string, '%Y-%m-%d %H:%M:%S.%f')))
+        timestamp = int(time.mktime(time.strptime(date_string,
+                                                   "%Y-%m-%d %H:%M:%S.%f")))
         data = recently_played_songs(start_time=timestamp)
         ti = context["ti"]
-        ti.xcom_push(key=f"my_data", value=data)
-
-
-
-
-
-
+        ti.xcom_push(key="my_data", value=data)
 
 
 def insert_data_from_xcom(databaseName, postgres_conn_id, **context):
     ti = context["ti"]
     data = ti.xcom_pull(key="my_data")
-    load_json_to_postgres(data=data, databaseName=databaseName, postgres_conn_id=postgres_conn_id)
-
-
-
+    load_json_to_postgres(
+        data=data, databaseName=databaseName, postgres_conn_id=postgres_conn_id
+    )
 
 
 default_args = {
-    "owner": "Kevin Nduati", 
+    "owner": "Kevin Nduati",
     "depends_on_past": False,
     # "start_date": start_time,
     "email_on_retry": False,
@@ -75,68 +76,58 @@ default_args = {
     "retries": 2,
     "retry_delay": timedelta(minutes=0.5),
     "max_active_runs": 1,
-    "execution_date_fn": get_next_execution_date
-
+    "execution_date_fn": get_next_execution_date,
 }
 # spotify does not store more than 50
 dag = DAG(
     "my_spotify_wrapped",
     default_args=default_args,
     start_date=days_ago(1),
-    schedule_interval= timedelta(hours=3) # you can define a cron job
+    schedule_interval=timedelta(hours=3),  # you can define a cron job
 )
 
 get_access_token = BashOperator(
-    task_id  = "get_access_token",
-    bash_command= "python3 '{0}'".format(refresh_token),
-    dag = dag
+    task_id="get_access_token",
+    bash_command="python3 '{0}'".format(refresh_token),
+    dag=dag,
 )
 
-      
 
 collect_songs_data = PythonOperator(
-    task_id = "collect_songs",
-    python_callable= get_recently_played_songs,
-    op_kwargs={
-    "postgres_conn_id": "postgres_conn"
-    },
+    task_id="collect_songs",
+    python_callable=get_recently_played_songs,
+    op_kwargs={"postgres_conn_id": "postgres_conn"},
     provide_context=True,
-    dag = dag
+    dag=dag,
 )
-
 
 
 insert_data = PythonOperator(
-    task_id = "insert_data",
-    python_callable = insert_data_from_xcom,
-    op_kwargs={
-        "databaseName": "spotify", 
-        "postgres_conn_id": "postgres_conn"
-    }, 
-    dag = dag
+    task_id="insert_data",
+    python_callable=insert_data_from_xcom,
+    op_kwargs={"databaseName": "spotify", "postgres_conn_id": "postgres_conn"},
+    dag=dag,
 )
 
-from datetime import date
 
 end_date = date.today().strftime("%Y-%m-%d")
 start_date = (date.today() - timedelta(days=7)).strftime("%Y-%m-%d")
 dbt_run = BashOperator(
     task_id="dbt_run",
-    bash_command="cd '" + dbt_folder + "' && dbt run --vars '{\"start_date\": \"" + start_date + "\", \"end_date\": \"" + end_date + "\"}'"
+    bash_command="cd '"
+    + dbt_folder
+    + '\' && dbt run --vars \'{"start_date": "'
+    + start_date
+    + '", "end_date": "'
+    + end_date
+    + "\"}'",
 )
-
 
 
 dbt_test = BashOperator(
-    task_id = "dbt_test",
-    bash_command = f"cd '{dbt_folder}' && dbt test"
+    task_id="dbt_test", bash_command=f"cd '{dbt_folder}' && dbt test"
 )
-
-
 
 
 # set up the dag procedure
 get_access_token >> collect_songs_data >> insert_data >> dbt_run >> dbt_test
- 
-
-
